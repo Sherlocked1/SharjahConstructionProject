e0ae79b937a8b49191a6e4ee88815a376df9792d
Mohammed Sayed <mohamadsayed7070@gmail.com>
2023-03-15 09:50:22 +0200

    cleaned up and added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhQNq_9UoVE57qbAXDHdDNilnWw4tv7QY",
+  authDomain: "sharjah-gov.firebaseapp.com",
+  projectId: "sharjah-gov",
+  storageBucket: "sharjah-gov.appspot.com",
+  messagingSenderId: "446017549487",
+  appId: "1:446017549487:web:91db799529866c9445905a",
+  measurementId: "G-PTPWZX727C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
